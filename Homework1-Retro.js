b8a3fcc9b665594ee796fcafd0c2ec6b8287a0e5
Artuxz <artuxz.sgafdre@gmail.com>
2018-11-22 13:25:02 +0000

    Retro Homework

---
+++ Homework1-Retro.js
@@ -0,0 +1,54 @@
+function setup() {
+  // Create the canvas
+  createCanvas(800, 600);
+  background(255);
+
+  // Set colors
+
+  fill (255, 0, 0, 255);
+  strokeWeight(5);
+  stroke(32, 13, 9);
+//Handle Conn1
+rect(60, 365, 70, 70);
+//handle Conn2
+rect(190, 365, 70, 70);
+  //handle
+  fill(193,197,198,255);
+  rect(100, 350, 120, 100);
+      //Cirlce Thingies
+    fill (255, 0, 0, 255);
+    ellipse(40, 400, 80, 80);
+    strokeWeight(3);
+    fill(255, 240, 0, 200);
+      ellipse(40, 400, 40, 40);
+      strokeWeight(5);
+      //Blade
+        fill(228, 228, 226, 200);
+      rect(340, 300, 450, 200);
+      //CenterP
+              fill(255, 240, 0, 255);
+          ellipse(550, 320, 120, 120);
+
+        //Preblade
+            fill(255, 240, 0, 200);
+          rect(260, 300, 80, 200);
+//          rect(400, 300, 80, 50);
+//          rect(400, 450, 80, 50);
+              //white
+              fill (255,255,255);
+                          triangle(790, 303, 790, 500, 600, 500);
+strokeWeight (0);
+                        triangle(800, 295, 790, 650, 600, 505);
+//text
+strokeWeight(3);
+
+              stroke(255, 240, 0, 255)
+line (350,490,490,350);
+line (350,470,485,330);
+line (350,450,475,310);
+line (350,430,460,310);
+line (350,410,440,310);
+line (350,390,420,310);
+line (350,360,400,310);
+line (350,340,380,310);
+}
