cc4a75367f9b9743e1685e4163929ddcf1096e57
Artuxz <artuxz.sgafdre@gmail.com>
2018-11-23 02:21:58 +0000

    Uploaded Assignment, organized everything

---
+++ Arturas-Vaitkus-GAD405-Assignment1/ArturasVaitkus-GAD405-Assignment1.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(500,500);
+    background(255,255,255);
+}
+  // Shape Draw
+function draw() {
+  customShape();
+  //Refresh
+  setTimeout(function() {
+  location.reload();
+}, 5000);
+   }
+  function customShape() {
+    let size = random(200);
+    let x = random(500);
+    let y = random(500);
+    //Size/Pos
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(x,y,size,size);
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(x,y,size*0.8,size*0.8);
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(x,y,size*0.6,size*0.6);
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(x,y,size*0.4,size*0.4);
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(x,y,size*0.2,size*0.2);
+  }
